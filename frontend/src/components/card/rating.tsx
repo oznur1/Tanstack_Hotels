c0a5148eabcf53c70804d147399760a6a99bcd8e
oznur1 <oznuroncel10@gmail.com>
2025-05-22 23:09:06 +0300

    uptated

--- frontend/src/components/card/rating.tsx
+++ frontend/src/components/card/rating.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import type{ FC } from 'react'
 
-const Rating = () => {
+
+interface Props{
+  rating:number;
+  expand:boolean;
+}
+const Rating:FC<Props> = ({rating,expand=false}) => {
+  //renk belirleme
+  const color:string=
+  rating >= 4.7
+  ? "bg-blue-500"
+  :rating >=4
+  ? "bg-green-500"
+  : rating >=3
+  ? "bg-yellow-500"
+  : "bg-red-500"
+
+  //metin belirle
+    const text=
+  rating >= 4.7
+  ? "Çok İyi"
+  :rating >=4
+  ? "İyi"
+  : rating >=3
+  ? "Orta"
+  : "Kötü"
+
   return (
     <div>
-      RATİNG
+    <span className={`${color} text-white p-2 rounded-lg font-bold w-fit`}>{rating}</span>
+  
+
+  {expand && <span className='fontsemibold text-lg ms-2'>{text}</span>}
     </div>
   )
 }
--- frontend/src/pages/home/filter.tsx
+++ frontend/src/pages/home/filter.tsx
@@ -1,10 +1,52 @@
 import type { FC } from "react"
+import { usePlaces } from "../../utils/service"
+import { sortOptions } from "../../utils/constants";
 
 const Filter:FC = () => {
+  
+  const {data}=usePlaces();
+  const locations=[...new Set(data?.map((i)=>i.location))];
+  
+
   return (
-    <div>
-      FİLTER
+   <form className="flex flex-col gap-4 lg:gap-10 lg:mt-15 lg:sticky lg:top-10">
+    <div className="field">
+      <label htmlFor="location">Nereye Gitmek İstiyorsunuz?</label>
+
+      <select className="input" name="location" id="location">
+      
+      <option value="">Seçiniz</option>
+       {locations?.map((location, index) => (
+  <option key={index} value={location}>
+    {location}
+  </option>
+))}
+</select>
     </div>
+     
+     <div className="field">
+      <label htmlFor="location">Konaklama yeri adına göre ara?</label>
+      <input  className ="input"
+      type="text"
+      name="location"
+      id="location"
+      placeholder="örn:Seaside Villa" />
+     </div>
+
+<div className="field">
+  <label htmlFor="sort">Sıralama Ölçütü</label>
+   <select className="input" name="sort" id="sort">
+    {sortOptions?.map((i, key) => (
+            <option key={key} value={i.value}>
+              {i.label}
+            </option>
+          ))}
+
+   </select>
+
+</div>
+   </form>
+
   )
 }
 
